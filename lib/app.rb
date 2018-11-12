73b68efcd400c76301c48fcfe90718933cac776b
alexandernewton <alex.newton@digital.cabinet-office.gov.uk>
2018-11-12 16:14:10 +0000

    Implemented index.erb

---
+++ lib/app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+# get '/' do
+#   "Hello World"
+# end
+#
+# get '/goodbye' do
+#   "Goodbye World"
+# end
+#
+# get'/secret' do
+#   "Rubber Duck"
+# end
+
+get '/cat' do
+erb(:index)
+end
